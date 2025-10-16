1c28887012566559093753d42942f5cc7fb81f2e
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-16 18:35:32 +0300

    feat: add yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="unique-script"
+          src="/script.js"
+          //           dangerouslySetInnerHTML={{
+          //             __html: `<script type="text/javascript">
+          //     (function(m,e,t,r,i,k,a){
+          //         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          //         m[i].l=1*new Date();
+          //         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          //         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          //     })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104671936', 'ym');
+
+          //     ym(104671936, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+          // </script>`,
+          //           }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
